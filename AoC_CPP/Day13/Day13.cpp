527450c92b8e99f3f2a3c3d04b400dd60fc1b246
Paweł Morawski <pawlo.morawski@gmail.com>
2021-12-16 20:49:39 +0100

    add solution for day13
    part1:
    read input, parse it and build board,
    fold it depends on axis and save
    results to new smaller board,
    count dots
    
    part2:
    print final board and read password

--- AoC_CPP/Day13/Day13.cpp
+++ AoC_CPP/Day13/Day13.cpp
@@ -7,8 +7,8 @@ namespace fs = std::filesystem;
 
 struct point
 {
-	point(const int y, const int x):
-	y(y), x(x)
+	point(const int y, const int x) :
+		x(x), y(y)
 	{ }
 
 	int x;
@@ -62,7 +62,7 @@ std::pair<std::vector<point>, std::vector<point>> parse_input(const std::string&
 		const auto first_number = std::stoi(first);
 		const auto second_number = std::stoi(second);
 		points.emplace_back(first_number, second_number);
-		std::cout << first << "," << second << std::endl;
+		//std::cout << first << "," << second << std::endl;
     }
 
 	std::vector<point> folds;
@@ -85,7 +85,7 @@ std::pair<std::vector<point>, std::vector<point>> parse_input(const std::string&
 			folds.emplace_back(0, number);
 		}
 
-		std::cout << first << "=" << second << std::endl;
+		//std::cout << first << "=" << second << std::endl;
     }
 
 	return std::make_pair(points, folds);
@@ -100,7 +100,7 @@ board create_board(const std::vector<point>& points)
 		max_x = std::max(point.x, max_x);
 		max_y = std::max(point.y, max_y);
 	}
-	board board_instance(max_x + 1, std::vector<char>(max_y + 1, '.'));
+	board board_instance(max_x + 1, std::vector(max_y + 1, '.'));
 	for (const auto& point : points)
 	{
 		board_instance[point.x][point.y] = '#';
@@ -121,13 +121,94 @@ void print_board(const board& board_instance)
 	}
 }
 
+int count_dots(const board& board_instance)
+{
+	auto dots_counter = 0;
+	for (const auto& board_line : board_instance)
+	{
+		for (const auto board_field : board_line)
+		{
+			if (board_field == '#')
+			{
+				dots_counter++;
+			}
+		}
+	}
+	return dots_counter;
+}
+
+board fold_board(const board& board_instance, const point& fold)
+{
+	board result;
+	if (fold.y == 0)
+	{
+		result = board(fold.x, std::vector(board_instance[0].size(), '.'));
+		for (int x = 0; x < static_cast<int>(board_instance.size()); x++)
+		{
+			for (int y = 0; y < static_cast<int>(board_instance[x].size()); y++)
+			{
+				if (board_instance[x][y] == '#')
+				{
+					if (x > fold.x)
+					{
+						const auto diff = x - fold.x;
+						const auto new_index = fold.x - diff;
+						result[new_index][y] = board_instance[x][y];
+					}
+					else if (x < fold.x)
+					{
+						result[x][y] = board_instance[x][y];
+					}
+		
+				}
+			}
+		}
+	}
+	else
+	{
+		result = board(board_instance.size(), std::vector(fold.y, '.'));
+		for (int x = 0; x < static_cast<int>(board_instance.size()); x++)
+		{
+			for (int y = 0; y < static_cast<int>(board_instance[x].size()); y++)
+			{
+				if (board_instance[x][y] == '#')
+				{
+					if (y > fold.y)
+					{
+						const auto diff = y - fold.y;
+						const auto new_index = fold.y - diff;
+						result[x][new_index] = board_instance[x][y];
+					}
+					else if (y < fold.y)
+					{
+						result[x][y] = board_instance[x][y];
+					}
+				}
+			}
+		}
+	}
+	return result;
+}
+
 int main()
 {
 	// part 1
-	auto result = read_file("input_day13_small.txt");
-	auto points_and_folds = parse_input(result);
-	auto board = create_board(points_and_folds.first);
-	print_board(board);
-	std::cout << "";
+	const auto result = read_file("input_day13.txt");
+	const auto [points, folds] = parse_input(result);
+	auto board = create_board(points);
+	//print_board(board);
+	std::cout << '\n';
+	for (const auto& fold : folds)
+	{
+		board = fold_board(board, fold);
+		std::cout << '\n';
+		//print_board(board);
+		const auto dots_counter = count_dots(board);
+		std::cout << '\n';
+		std::cout << dots_counter;
+		std::cout << '\n';
+	}
+
 	// part 2
+	print_board(board);
 }
